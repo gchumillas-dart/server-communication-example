44cc476e18a7900304271981499f00837f25c432
soloproyectos <gonzalo.chumillas@gmail.com>
2016-06-01 00:03:41 +0200

    replace property name

--- client/lib/components/edit_item_component.dart
+++ client/lib/components/edit_item_component.dart
@@ -9,20 +9,20 @@ import 'package:list/entities/item_entity.dart';
 
 @Component(selector: 'div.edit-item', templateUrl: 'edit_item_component.html')
 class EditItemComponent implements OnInit {
-  final EditService _listService;
+  final EditService _editService;
   final RouteParams _params;
   ItemEntity item;
 
-  EditItemComponent(this._listService, this._params);
+  EditItemComponent(this._editService, this._params);
 
   Future<Null> ngOnInit() async {
     // fetches the item from db
     final id = _params.get('id');
-    item = await _listService.getItemById(id);
+    item = await _editService.getItemById(id);
   }
 
   Future<Null> onSubmit() async {
-    await _listService.editItem(item);
+    await _editService.editItem(item);
     goBack();
   }
 
